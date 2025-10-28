59ef3ff40ffc2d55f5308825e5e085867482d30c
brkim2008 <brkim2008@hanmail.net>
2025-10-28 18:29:23 +0900

    Merge remote-tracking branch 'origin/main' into main

--- src/banking/AccountManager.java
+++ src/banking/AccountManager.java
@@ -4,13 +4,12 @@ package banking;
  컨트롤 클래스로써 프로그램의 전반적인 기능을 구현
  - 배열선언
  - Scanner 클래스 선언 & 표준입력 스레드와 연결하여 사용자 입력준비
- - 1. 계좌계설
+ - 
    1.계좌개설 - MakeAccount() 메서드, try~catch, 공통정보 정의
    2.입  금  - Deposit() 메서드, try~catch
    3.출  금  - withdraw() 메서드, try~catch
    4.전체계좌출력  - ShowAllAccountInfo() 메서드
-   5.프로그램종료
- * */
+   5.프로그램종료                                       */
 
 import java.util.ArrayList;
 import java.util.InputMismatchException;
--- src/banking/ICustomDefine.java
+++ src/banking/ICustomDefine.java
@@ -5,4 +5,15 @@ package banking;
    - 고객의 신용등급별 이자율 : A, B, C _ 7%, 4%, 2%   */
 
 public interface ICustomDefine {
+	
+	int MENU_MAKE = 1;
+	int MENU_DEPOSIT = 2;
+	int MENU_WITHDRAW = 3;
+	int MENU_INQUIRE = 4;
+	int MENU_EXIT = 5;
+	
+	double GRADE_A = 0.07;
+	double GRADE_B = 0.05;
+	double GRADE_C = 0.03;
+	
 }
